57a74954048c60e884462e2338b2dd226b56ac29
Puneet Kaur <80326308+puneetkaur2929@users.noreply.github.com>
2022-02-02 22:11:09 +0530

    Time: 4 ms (46.07%), Space: 10.1 MB (7.66%) - LeetHub

--- 100-same-tree/100-same-tree.cpp
+++ 100-same-tree/100-same-tree.cpp
@@ -15,6 +15,6 @@ public:
         if(p==NULL || q==NULL){
             return p==q;
         }
-        return (p->val==q->val) && isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+        return isSameTree(p->left,q->left ) && (p->val==q->val) && isSameTree(p->right,q->right);
     }
 };
