8cd58a71a0c53c6b79b37490091f470d8eaf78d8
Puneet Kaur <80326308+puneetkaur2929@users.noreply.github.com>
2022-02-04 23:11:32 +0530

    Time: 7 ms (54.55%), Space: 16.5 MB (17.93%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.cpp
+++ 101-symmetric-tree/101-symmetric-tree.cpp
@@ -13,7 +13,7 @@ class Solution {
 public:
     bool inor(TreeNode* t1,TreeNode* t2){
         if(t1==NULL || t2==NULL){
-            return t1==t2;
+            return t1==t2?true:false;
         }
         if(t1->val!=t2->val){
             return false;
