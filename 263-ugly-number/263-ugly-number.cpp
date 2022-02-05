42fbfccac37fe7f35f612b4b5d45d0f8a5df01ba
Puneet Kaur <80326308+puneetkaur2929@users.noreply.github.com>
2022-02-05 20:01:34 +0530

    Time: 3 ms (48.78%), Space: 5.9 MB (20.46%) - LeetHub

---
+++ 263-ugly-number/263-ugly-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+       if(n==0){
+           return false;
+       }
+        while(n!=0){
+            if(n==1){
+            return true;
+        } 
+            if(n==0){
+           return false;
+       }
+           n=n%2==0 ? n/2 :(n%3==0 ? n/3 : (n%5==0  ? n/5 : -1));
+            if(n==-1){
+                return false;
+            }
+            
+        }
+        return true;
+    }
+};
