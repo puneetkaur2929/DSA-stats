27b7499f75e9c429457347f0f8959021c4eb44ed
Puneet Kaur <80326308+puneetkaur2929@users.noreply.github.com>
2022-02-25 23:35:03 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (19.06%) - LeetHub

---
+++ 165-compare-version-numbers/165-compare-version-numbers.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        stringstream s1(version1);
+        stringstream s2(version2);
+        string tok1,tok2;
+        vector<int>v1;
+        vector<int>v2;
+        while(getline(s1,tok1,'.')){
+            v1.push_back(stoi(tok1));
+        }
+        while(getline(s2,tok2,'.')){
+            v2.push_back(stoi(tok2));
+        }
+       int i=0;
+        int j=0;
+        int c1=0;
+        int c2=0;
+        // cout<<" size "<<v1.size() <<" "<<v2.size()<<endl;
+        while(i<v1.size() && j<v2.size()) {
+           if(v1[i]<v2[j]){
+               return -1;
+           }
+            else if(v1[i]>v2[j]){
+                return 1;
+            }
+            i++;
+            j++;
+        }
+       
+        if(i!=v1.size()){
+            while(i<v1.size()){
+                 // cout<<v1[i]<<" "<<endl;
+               if( v1[i]>0 ){
+                  return 1;
+               }
+            i++;
+            }
+          
+        }
+         if(j!=v2.size()){
+           while(j<v2.size()){
+                if( v2[j]>0 ){
+                 return -1;
+               }
+            j++;
+           }
+              
+        }
+        return 0;
+       
+    }
+};
